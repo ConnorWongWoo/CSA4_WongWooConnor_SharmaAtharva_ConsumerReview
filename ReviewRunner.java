c22af325c41b84bc506bdcf473e59fea6578d236
ConnorWongWoo <wongwoo3874@mydusd.org>
2025-10-20 12:38:36 -0700

    Final qualitative statement

--- ReviewRunner.java
+++ ReviewRunner.java
@@ -9,7 +9,7 @@
 
 class ReviewRunner {
   
-  public static float getSum(String data) {
+  public static float getSentiment(String data) {
     float sum = 0.0f;
     String[] words = data.split(" ");
     for ( String w: words) {
@@ -22,10 +22,17 @@ class ReviewRunner {
     // Used chatgpt to properly split the reviews after the date
     String[] data = Review.textToString("data.txt").split("(?<=\\b\\d{4}\\b)"); 
     int i = 0;
+    float avg_sentiment = 0.0f;
     for (String review: data) {
-
-      System.out.println("Review " + i + "| Sentiment Score: " + getSum(review) + " | " + review);
+      float review_sentiment = getSentiment(review); 
+      System.out.println("Review " + i + "| Sentiment Score: " + review_sentiment + " | " + review);
+      avg_sentiment += review_sentiment;
       i++;
     }
+    avg_sentiment = avg_sentiment / i;
+    System.out.println("The average sentiment of the reviews of the movie Ironman (2008) is: " + avg_sentiment + ".");
+    if (avg_sentiment > 7.0f) System.out.println("The reviews were mostly positive! People loved the movie!");
+    else if (4.0f < avg_sentiment) System.out.println("The reviews were somewhat positive. People liked this movie.");
+    else System.out.println("The reviews were pretty bad... People did not like this movie.");
   }
 }
