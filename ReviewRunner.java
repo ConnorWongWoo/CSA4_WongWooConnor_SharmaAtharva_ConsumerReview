565e02c4baa2746c18f9d41d7890f2254953ce77
ConnorWongWoo <wongwoo3874@mydusd.org>
2025-10-20 12:30:19 -0700

    Restructured how the main method works to work with the format of reviews

--- ReviewRunner.java
+++ ReviewRunner.java
@@ -1,5 +1,3 @@
-import java.util.ArrayList;
-import java.lang.Math;
 /*
  * Requirements:
  * Write a program with a main method. (Complete)
@@ -10,44 +8,24 @@ import java.lang.Math;
  */
 
 class ReviewRunner {
-  public static ArrayList<Integer> wordIndexes(String word, String[] data) {
-    ArrayList<Integer> wordIndexes = new ArrayList<>();
-    for (int i = 0; i < data.length; i++) {
-      if (data[i].contains(word)) {
-        // System.out.println("Word: " + data[i] + "Index: " + i);
-        wordIndexes.add(i);
-      }
-    }
-    return wordIndexes;
-  }
   
-  public static float getSum(int index, int range, String[] data) {
+  public static float getSum(String data) {
     float sum = 0.0f;
-    for (int i = Math.max(0, index - range); i < Math.min(index + range, data.length); i++) {
-      sum += Review.sentimentVal(Review.removePunctuation(data[i]));
+    String[] words = data.split(" ");
+    for ( String w: words) {
+      sum += Review.sentimentVal(w);
     }
-    return sum;
+    return (float) (Math.round(sum * 100f) / 100f);
   }
 
   public static void main(String[] args) {
-    String[] data = Review.textToString("data.txt").split(" ");
-    String[] marvel_words = {"Marvel", "MCU"};
-    String[] dc_words = {"DC", "DCEU"};
-    // ArrayList<ArrayList<Integer>> total_indexes = new ArrayList<>();
-    for (String w: marvel_words) {
-      ArrayList<Integer> w_l = wordIndexes(w, data);
-      for (int num: w_l) {
-        System.out.println("Word: "+ w + " Index: " + num + " Total Sentiment: " + getSum(num, 20, data));
-      }
-      // total_indexes.add(w_l);
-    }
-    // for (String w: dc_words) {
-    //   ArrayList<Integer> w_l = wordIndexes(w, data);
-    //   // total_indexes.add(w_l);
-    // }
+    // Used chatgpt to properly split the reviews after the date
+    String[] data = Review.textToString("data.txt").split("(?<=\\b\\d{4}\\b)"); 
+    int i = 0;
+    for (String review: data) {
 
-    // for (String w: data) {
-    //   w = Review.removePunctuation(w);
-    // }
+      System.out.println("Review " + i + "| Sentiment Score: " + getSum(review) + " | " + review);
+      i++;
+    }
   }
 }
