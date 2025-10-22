d1f11daac1586cfa5deb42ca201cb63e3a9273eb
ConnorWongWoo <wongwoo3874@mydusd.org>
2025-10-22 11:19:28 -0700

    Updatd how the program parses the data to only use the review to find the average sentiment as before it would also include the author's name, publication, and either 'fresh score' or 'rotten score'

--- ReviewRunner.java
+++ ReviewRunner.java
@@ -15,24 +15,26 @@ class ReviewRunner {
     for ( String w: words) {
       sum += Review.sentimentVal(w);
     }
-    return (float) (Math.round(sum * 100f) / 100f);
+    return sum;
   }
 
   public static void main(String[] args) {
     // Used chatgpt to properly split the reviews after the date
-    String[] data = Review.textToString("data.txt").split("(?<=\\b\\d{4}\\b)"); 
+    String[] data = Review.textToString("data.txt").split("(?=(?:Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)[a-z]*\\s+\\d{1,2},\\s*\\d{4})");
+
     int i = 0;
     float avg_sentiment = 0.0f;
-    for (String review: data) {
+    for (i = 0; i < data.length-1; i++) {
+      String full_review = Review.removePunctuation(data[i]);
+      String review = full_review.split("score\\.")[1];
       float review_sentiment = getSentiment(review); 
-      System.out.println("Review " + i + "| Sentiment Score: " + review_sentiment + " | " + review);
+      System.out.println("Review " + i + " |" + review + "\n");
       avg_sentiment += review_sentiment;
-      i++;
     }
     avg_sentiment = avg_sentiment / i;
     System.out.println("The average sentiment of the reviews of the movie Ironman (2008) is: " + avg_sentiment + ".");
-    if (avg_sentiment > 7.0f) System.out.println("The reviews were mostly positive! People loved the movie!");
-    else if (4.0f < avg_sentiment) System.out.println("The reviews were somewhat positive. People liked this movie.");
+    if (avg_sentiment > 3.5f) System.out.println("The reviews were mostly positive! People loved the movie!");
+    else if (2.5f < avg_sentiment) System.out.println("The reviews were somewhat positive. People liked this movie.");
     else System.out.println("The reviews were pretty bad... People did not like this movie.");
   }
 }
